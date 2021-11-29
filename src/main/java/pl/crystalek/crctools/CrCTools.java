d544ea90f913638c69d7158b18d2dedd072668e3
Crystal <janwolowski@crystalek.pl>
2021-11-29 19:20:04 +0100

    Initial Commit

---
+++ src/main/java/pl/crystalek/crctools/CrCTools.java
@@ -0,0 +1,17 @@
+package pl.crystalek.crctools;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CrCTools extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
